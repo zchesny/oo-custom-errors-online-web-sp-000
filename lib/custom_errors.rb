b8850ffd44d431ed5bd30aad7df5003e36737283
Zoey Chesny <z.chesny@gmail.com>
2020-04-15 23:47:23 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,9 +7,9 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.class != Person 
-      raise PartnerError 
-    else 
+    if person.class != Person
+      raise PartnerError
+    else
       person.partner = self
     end
   end
